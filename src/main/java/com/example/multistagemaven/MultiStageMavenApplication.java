a6f109d476fccbfd989a4d149043aedbcd8d61da
DimaPushka <wwaassuupp777@gmailcom>
2022-12-04 17:11:47 +0300

    тест

---
+++ src/main/java/com/example/multistagemaven/MultiStageMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.multistagemaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiStageMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiStageMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/multistagemaven/MultiStageMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multistagemaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiStageMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
